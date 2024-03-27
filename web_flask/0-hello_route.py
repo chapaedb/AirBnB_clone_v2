2c8a8e570b2e02032ff886e1178dd786308b3cf3
chapaedb <chapaeresso1@gmail.com>
2024-03-27 07:47:59 -0700

    Update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,15 @@
+#!/usr/bin/python3
+"""
+A simple Flask web application.
+"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
+def hello_hbnb():
     return 'Hello HBNB!'
 
 
